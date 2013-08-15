08b2bc93e47fdd016147a45f7904997bad92a0f3
Pippin Williamson <pippin@pippinsplugins.com>
2013-08-14 19:31:57 -0500

    First file

---
+++ edd-recount-earnings.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Plugin Name: EDD Recount Earnings
+ * Description: Allows you to recalculate the earnings of products in EDD. Useful if product earnings get off somehow
+ * Author: Pippin Williamson
+ * Version: 0.1
+ */
+
+class EDD_Recount_Earnings {
+
+	public function __construct() {
+
+	}
+
+	
+}
+new EDD_Recount_Earnings;
