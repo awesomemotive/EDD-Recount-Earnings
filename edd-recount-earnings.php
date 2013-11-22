159663381b662ece7f660058fc842bd95059ced2
Pippin Williamson <pippin@pippinsplugins.com>
2013-11-22 10:34:56 -0600

    Add Recount Store Earnings option to the Tools page

--- edd-recount-earnings.php
+++ edd-recount-earnings.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Plugin Name: EDD Recount Earnings
+ * Plugin Name: Easy Digital Downloads - Recount Earnings
  * Description: Allows you to recalculate the earnings of products in EDD. Useful if product earnings get off somehow
  * Author: Pippin Williamson
  * Version: 0.1
@@ -10,7 +10,9 @@ class EDD_Recount_Earnings {
 
 	public function __construct() {
 		add_action( 'edd_stats_meta_box', array( $this, 'stats_box_link' ) );
+		add_action( 'edd_tools_after', array( $this, 'tools_page' ) );
 		add_action( 'edd_recount_earnings', array( $this, 'recount' ) );
+		add_action( 'edd_recount_store_earnings', array( $this, 'recount_store_earnings' ) );
 	}
 
 	public function stats_box_link() {
@@ -29,6 +31,23 @@ class EDD_Recount_Earnings {
 				echo '<a href="' . add_query_arg( $args, $base_url ) . '">' . __( 'Recount Earnings', 'edd-recount-earnings' ) . '</a>';
 			echo '</td>';
 		echo '</tr>';
+	}
+
+	public function tools_page() {
+?>
+	<div class="postbox">
+		<h3><span><?php _e( 'Recount Store Earnings', 'edd' ); ?></span></h3>
+		<div class="inside">
+			<p><?php _e( 'Use this tool to recount your store\'s total earnings in the case they have become incorrect.', 'edd' ); ?></p>
+			<form method="post" action="<?php echo admin_url( 'edit.php?post_type=download&page=edd-tools' ); ?>">
+				<p><input type="hidden" name="edd_action" value="recount_store_earnings" /></p>
+				<p>
+					<?php submit_button( __( 'Recount Earnings', 'edd' ), 'secondary', 'submit', false ); ?>
+				</p>
+			</form>
+		</div><!-- .inside -->
+	</div><!-- .postbox -->
+<?php		
 	}
 
 	public function recount() {
@@ -83,6 +102,46 @@ class EDD_Recount_Earnings {
 		$base_url = admin_url( 'post.php' );
 
 		wp_redirect( add_query_arg( $args, $base_url ) ); exit;
+
+	}
+
+	public function recount_store_earnings() {
+
+		global $edd_logs, $wpdb;
+
+		if( ! current_user_can( 'view_shop_reports' ) )
+			wp_die( 'Cheating' );
+
+		$total = (float) 0;
+
+		$args = apply_filters( 'edd_get_total_earnings_args', array(
+			'offset' => 0,
+			'number' => -1,
+			'mode'   => 'live',
+			'status' => array( 'publish', 'revoked' ),
+			'fields' => 'ids'
+		) );
+
+		$payments = edd_get_payments( $args );
+		if ( $payments ) {
+			foreach ( $payments as $payment ) {
+				$total += edd_get_payment_amount( $payment );
+			}
+		}
+
+		// Cache results for 1 day. This cache is cleared automatically when a payment is made
+		set_transient( 'edd_earnings_total', $total, 86400 );
+
+		if( $total < 0 ) {
+			$total = 0; // Don't ever show negative earnings
+		}
+
+		$total = round( $total, 2 );
+
+		// Store the total for the first time
+		update_option( 'edd_earnings_total', $total );
+
+		wp_redirect( admin_url( 'edit.php?post_type=download&page=edd-tools' ) ); exit;
 
 	}
 
